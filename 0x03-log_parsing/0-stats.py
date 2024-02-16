e2862b3fc44093d71ac2b67c80f21c63b93b0933
Elocodes <elochukwuodoeze@gmail.com>
2024-02-15 19:16:44 -0800

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,54 @@
 #!/usr/bin/python3
-
+"""
+log parsing
+"""
 import sys
+import signal
 
-totalfile_size = 0
-status_dict = {}
 
-try:
-    for i, line in enumerate(sys.stdin, start=1):
-        try:
-            items = line.split()
-            ip_addr = items[0]
-            date = items[3][1:-1]
-            status_code = int(items[8])
-            file_size = int(items[9])
+def print_metrics(total_size, status_counts):
+    """ metrics """
+    print("Total file size: File size: {}".format(total_size))
+    for code in sorted(status_counts):
+        print("{}: {}".format(code, status_counts[code]))
 
-            totalfile_size += file_size
-            # keep track of how many times each status cod eappears
-            status_dict[status_code] = status_dict.get(status_code, 0) + 1
-            # print output afetr 10 lines
-            if i % 10 == 0: #len(status_dict) == 10:
-                print ("File size: {}".format(totalfile_size))
-                for k, v in sorted(status_dict.items()):
-                    print("{}: {}".format(k, v))
-                #status_dict.clear()
-                sys.stdout.flush()
-        except (IndexError, ValueError):
-            line.strip()
-except KeyboardInterrupt:
-    # print statistics if ctrl c is pressed
-    print ("File size: {}".format(totalfile_size))
-    for k, v in sorted(status_dict.items()):
-        print("{}: {}".format(k, v))
-    sys.exit(0)
+
+def process_line(line, total_size, status_counts):
+    """ print each line """
+    parts = line.split()
+    if len(parts) >= 9 and parts[8].isdigit():
+        file_size = int(parts[8])
+        total_size += file_size
+
+        status_code = parts[7]
+        if status_code.isdigit():
+            status_code = int(status_code)
+            if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
+                status_counts[status_code] += 1
+
+    return total_size, status_counts
+
+
+def main():
+    """ main func """
+    total_size = 0
+    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0,
+                     500: 0}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            total_size, status_counts = process_line(line, total_size,
+                                                     status_counts)
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_metrics(total_size, status_counts)
+
+    except KeyboardInterrupt:
+        print_metrics(total_size, status_counts)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
