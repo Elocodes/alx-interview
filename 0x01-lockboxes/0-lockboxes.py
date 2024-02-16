84add3812c2480cef180411b9e8d756a1a5857b1
Elocodes <elochukwuodoeze@gmail.com>
2024-02-15 18:27:04 -0800

    boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+unlock boxes
+"""
+
+
+def canUnlockAll(boxes):
+    """ determine if boxes given can be opened """
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
